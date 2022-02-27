a77e5df7e0b7e0ea7c482e91efaa2f65d3b6b58b
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-02-27 15:57:14 +0100

    Created interface for SyntaxTree building

---
+++ formula2cnf/Formulas/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formula2cnf.Formulas
+{
+    internal sealed class Node
+    {
+        public enum NodeType : byte { And, Or, Not, Variable }
+        public readonly NodeType Type;
+        public readonly IReadOnlyList<Node> Children;
+
+        public Node(NodeType type, IReadOnlyList<Node> children)
+        {
+            Type = type;
+            Children = children;
+        }
+    }
+}
