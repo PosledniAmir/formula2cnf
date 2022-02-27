39f704b4c04e72c856a58c6919ec5e582b198e5b
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-02-27 22:45:48 +0100

    Updated Node class, removed ISyntaxTreeBuilder - I need to reconsider this class

--- formula2cnf/Formulas/Node.cs
+++ formula2cnf/Formulas/Node.cs
@@ -8,14 +8,39 @@ namespace formula2cnf.Formulas
 {
     internal sealed class Node
     {
-        public enum NodeType : byte { And, Or, Not, Variable }
-        public readonly NodeType Type;
-        public readonly IReadOnlyList<Node> Children;
+        public enum NodeType : byte { And, Or, Not, Variable, Invalid }
+        private readonly List<Node> _children;
+        private NodeType _type;
+        private Node? _parent;
+        public NodeType Type => _type;
+        public IReadOnlyList<Node> Children => _children;
+        public Node? Parent => _parent;
 
-        public Node(NodeType type, IReadOnlyList<Node> children)
+        public Node()
         {
-            Type = type;
-            Children = children;
+            _parent = null;
+            _children = new List<Node>();
+            _type = NodeType.Invalid;
+        }
+
+        private Node(Node parent) : this()
+        {
+            _parent = parent;
+        }
+
+        public bool TrySetType(NodeType type)
+        {
+            if (_type == NodeType.Invalid)
+            {
+                _type = type;
+                return true;
+            }
+            return false;
+        }
+
+        public void AddChild()
+        {
+            _children.Add(new Node(this));
         }
     }
 }
