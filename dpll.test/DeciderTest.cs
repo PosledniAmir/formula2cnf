3f838fa5828d4103af82243d99ee2ff6cea9966e
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-03-18 15:04:38 +0100

    Test project added

---
+++ dpll.test/DeciderTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace dpll.test
+{
+    public class DeciderTest
+    {
+        [Fact]
+        public void BasicTest01()
+        {
+
+        }
+    }
+}
--- dpll/Program.cs
+++ dpll/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("dpll.test")]
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
