ced0e3d3de415fb480a7cf0991783e073d8feb4f
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-08-01 14:26:43 +0200

    builder done

---
+++ n-queens/Builders/TableBuilder.cs
@@ -0,0 +1,28 @@
+﻿using n_queens.Formulas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_queens.Builders
+{
+    internal sealed class TableBuilder
+    {
+        private IReadOnlyList<RowAtBuilder> _rows;
+        public int Size => _rows.Count;
+
+        public TableBuilder(int size)
+        {
+            _rows = Enumerable
+                .Range(0, size)
+                .Select(y => new RowAtBuilder(size, y))
+                .ToList();
+        }
+
+        public Formula Build()
+        {
+            return FormulaMerger.Merge(Op.And, _rows.Select(r => r.Build()).ToList());
+        }
+    }
+}
