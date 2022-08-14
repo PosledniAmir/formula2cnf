7ba95e01f603e38e56f609999767f82728068b26
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-08-14 16:42:22 +0200

    Preparing for all solutions

---
+++ cryptoarithmetics/InstanceSolver.cs
@@ -0,0 +1,117 @@
+﻿using cryptoarithmetics.Parsing;
+using Microsoft.Z3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptoarithmetics
+{
+    internal sealed class InstanceSolver : IDisposable
+    {
+        public readonly string Instance;
+        private readonly Context _context;
+        private ConditionGenerator? _generator;
+        private Solver? _solver;
+
+        public InstanceSolver(string instance)
+        {
+            Instance = instance;
+            _context = new Context();
+            _generator = null;
+            _solver = null;
+        }
+
+        private ConditionGenerator CreateGenerator()
+        {
+            var builder = new FormulaBuilder(_context, 10);
+
+            var tokens = Tokenizer
+                .TokenizeInstance(Instance)
+                .ToList();
+
+            var parsed = Parser.Parse(tokens);
+            return new ConditionGenerator(builder, parsed);
+        }
+
+        private Solver PepareSolver(ConditionGenerator generator)
+        {
+            var ranges = generator.CreateRangeConditions();
+            var main = generator.CreateInstanceCondition(true);
+
+            var solver = _context.MkSolver();
+            solver.Assert(ranges.ToArray());
+            solver.Assert(main);
+
+            return solver;
+        }
+
+        private StringBuilder AppendModel(StringBuilder builder, Model model, ConditionGenerator generator)
+        {
+            var map = new Dictionary<char, char>();
+            foreach (var (name, constant) in generator.Constants)
+            {
+                var solution = model.Evaluate(constant);
+                map.Add(name.First(), solution.ToString().First());
+                builder.AppendLine($"{name} = {solution}");
+            }
+
+            var solved = Instance.Select(c =>
+            {
+                if (map.TryGetValue(c, out var r))
+                {
+                    return r;
+                }
+                else
+                {
+                    return c;
+                }
+            }).Aggregate(new StringBuilder(), (builder, c) => builder.Append(c))
+            .ToString();
+
+            return builder.AppendLine(solved);
+        }
+
+        private Tuple<int, string> SolveInternal()
+        {
+            _generator ??= CreateGenerator();
+            _solver ??= PepareSolver(_generator);
+
+            var status = _solver.Check();
+            var builder = new StringBuilder().AppendLine($"SAT status: {status}");
+
+            if (status == Status.SATISFIABLE)
+            {
+                var model = _solver.Model;
+                builder = AppendModel(builder, model, _generator);
+                return Tuple.Create(0, builder.ToString());
+            }
+            else if (status == Status.UNSATISFIABLE)
+            {
+                return Tuple.Create(0, builder.ToString());
+            }
+            else
+            {
+                return Tuple.Create(1, builder.AppendLine(_solver.ReasonUnknown).ToString());
+            }
+        }
+
+        public Tuple<int, string> Solve()
+        {
+            try
+            {
+                return SolveInternal();
+            }
+            catch (Exception ex)
+            {
+                return Tuple.Create(1, $"Error: {ex}");
+            }
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)_context).Dispose();
+        }
+    }
+}
--- cryptoarithmetics/Program.cs
+++ cryptoarithmetics/Program.cs
@@ -7,56 +7,7 @@ using System.Text;
 [assembly: InternalsVisibleTo("cryptoarithmetics.test")]
 
 var instance = "(SEND + MORE = MONEY) || (SQUARE - DANCE = DANCER)";
-using var context = new Context();
-var builder = new FormulaBuilder(context, 10);
-
-var tokens = Tokenizer
-    .TokenizeInstance(instance)
-    .ToList();
-
-var parsed = Parser.Parse(tokens);
-var generator = new ConditionGenerator(builder, parsed);
-
-var ranges = generator.CreateRangeConditions();
-var main = generator.CreateInstanceCondition(true);
-var solver = context.MkSolver();
-solver.Assert(ranges.ToArray());
-solver.Assert(main);
-var status = solver.Check();
-Console.WriteLine($"SAT status: {status}");
-if (status == Status.SATISFIABLE)
-{
-    var map = new Dictionary<char, char>();
-    var model = solver.Model;
-    foreach (var (name, constant) in generator.Constants)
-    {
-        var solution = model.Evaluate(constant);
-        map.Add(name.First(), solution.ToString().First());
-        Console.WriteLine($"{name} = {solution}");
-    }
-
-    var solved = instance.Select(c =>
-    {
-        if (map.TryGetValue(c, out var r))
-        {
-            return r;
-        }
-        else
-        {
-            return c;
-        }
-    }).Aggregate(new StringBuilder(), (builder, c) => builder.Append(c))
-    .ToString();
-
-    Console.WriteLine(solved);
-
-    return 0;
-}
-else if (status == Status.UNSATISFIABLE)
-{
-    return 0;
-}
-else
-{
-    return 1;
-}
+using var solver = new InstanceSolver(instance);
+var result = solver.Solve();
+Console.Write(result.Item2);
+return result.Item1;
