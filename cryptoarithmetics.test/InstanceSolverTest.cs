f2293272364ee31f9fbe0390fa3cf27037e00c2e
Fl0ctioncers <Fl0ctioncers@outlook.com>
2022-08-15 13:24:57 +0200

    Added some tests

---
+++ cryptoarithmetics.test/InstanceSolverTest.cs
@@ -0,0 +1,93 @@
+﻿using cryptoarithmetics.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptoarithmetics.test
+{
+    public sealed class InstanceSolverTest
+    {
+        public struct TestData
+        {
+            public string Instance { get; set; }
+            public int Base { get; set; }
+            public bool Unique { get; set; }
+            public int MaxSolutions { get; set; }
+            public IReadOnlyList<string> Solutions { get; set; }
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[]
+            {
+                new TestData {
+                    Instance = "SEND + MORE = MONEY",
+                    Base = 2,
+                    Unique = false,
+                    MaxSolutions = 3,
+                    Solutions = new List<string>
+                    {
+                        "1110 + 1001 = 10111",
+                        "1010 + 1010 = 10100",
+                        "1001 + 1000 = 10001",
+                    },
+                }
+            };
+            yield return new object[]
+            {
+                new TestData {
+                    Instance = "SEND + MORE = MONEY",
+                    Base = 10,
+                    Unique = true,
+                    MaxSolutions = 3,
+                    Solutions = new List<string>
+                    {
+                        "9567 + 1085 = 10652",
+                    },
+                }
+            };
+            yield return new object[]
+            {
+                new TestData {
+                    Instance = "(SEND + MORE = MONEY) && (SQUARE - DANCE = DANCER)",
+                    Base = 16,
+                    Unique = false,
+                    MaxSolutions = 3,
+                    Solutions = new List<string>
+                    {
+                        "(F00F + 1000 = 1000F) && (FF0000 - F0000 = F00000)",
+                    },
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void BasicTest01(TestData input)
+        {
+            var solver = new InstanceSolver(input.Instance, input.Base, input.Unique);
+            var solutions = new List<string>();
+            var tries = input.MaxSolutions;
+            while (solver.CanContinue && tries > 0)
+            {
+                var (good, result) = solver.Solve();
+                if (good == 0)
+                {
+                    var splitted = result.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (splitted.Length > 1)
+                    {
+                        solutions.Add(splitted[^1]);
+                    }
+                }
+                tries--;
+            }
+            Assert.Equal(input.Solutions.Count, solutions.Count);
+            for (var i = 0; i < solutions.Count; ++i)
+            {
+                Assert.Equal(input.Solutions[i], solutions[i]);
+            }
+        }
+    }
+}
--- cryptoarithmetics/Program.cs
+++ cryptoarithmetics/Program.cs
@@ -1,16 +1,125 @@
 ﻿using cryptoarithmetics;
 using cryptoarithmetics.Parsing;
 using Microsoft.Z3;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 [assembly: InternalsVisibleTo("cryptoarithmetics.test")]
 
-var computeSolutions = new AtMost(1_000);
+const string Help = @"Arguments could not be parsed.
+Cryptoarithmetics usage: cryptoarithmetics [input] [--unique | -u] -k <base> -c <computeSize> -p <printSize>
+[input] = text file with cryptoarithmetics instance
+-u, -unique = specifies if solution should have unique digit per letter per satisfying clauses
+<base> = base of the solution
+<computeSize> = number of solutions computed
+<printSize> = number of solutions printed";
+
+var watch = new Stopwatch();
+watch.Start();
+var help = false;
+var nextBase = false;
+var nextCompute = false;
+var nextPrint = false;
+var computeSolutions = new AtMost(100);
 var printSolutions = new AtMost(3);
-var instance = "(SEND + MORE = MONEY) || (SQUARE - DANCE = DANCER)";
-using var solver = new InstanceSolver(instance, 3, false);
+var unique = false;
+var k = 16;
+var instance = "(SEND + MORE = MONEY) && (SQUARE - DANCE = DANCER)";
+
+foreach (var arg in args)
+{
+    var lower = arg.ToLower();
+    if (nextBase)
+    {
+        nextBase = false;
+        if (!int.TryParse(arg, out k))
+        {
+            help = true;
+            break;
+        }
+        continue;
+    }
+    else if (nextCompute)
+    {
+        nextCompute = false;
+        if (lower == "all")
+        {
+            computeSolutions = new AtMost(true);
+        }
+        else if (int.TryParse(arg, out var computeInt))
+        {
+            computeSolutions = new AtMost(computeInt);
+        }
+        else
+        {
+            help = true;
+            break;
+        }
+        
+        continue;
+    }
+    else if (nextPrint)
+    {
+        nextCompute = false;
+        if (lower == "all")
+        {
+            printSolutions = new AtMost(true);
+        }
+        else if (int.TryParse(arg, out var computeInt))
+        {
+            printSolutions = new AtMost(computeInt);
+        }
+        else
+        {
+            help = true;
+            break;
+        }
+
+        continue;
+    }
+    else if  (lower == "--unique")
+    {
+        unique = true;
+    }
+    else if (lower == "-s")
+    {
+        unique = true;
+    }
+    else if (File.Exists(arg))
+    {
+        instance = File.ReadAllText(arg);
+    }
+    else if (lower == "-k")
+    {
+        nextBase = true;
+        continue;
+    }
+    else if (lower == "-c")
+    {
+        nextCompute = true;
+        continue;
+    }
+    else if (lower == "-p")
+    {
+        nextPrint = true;
+        continue;
+    }
+    else
+    {
+        help = true;
+    }
+}
+
+if (help)
+{
+    Console.WriteLine(Help);
+    return 1;
+}
+
+using var solver = new InstanceSolver(instance, k, unique);
 var returnValue = 0;
+
 while (solver.CanContinue && printSolutions.CanContinue)
 {
     var result = solver.Solve();
